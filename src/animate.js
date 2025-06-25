098e368b46a1fab60526bc2b0753f7381086a37a
Helen <helen.luanml@gmail.com>
2025-06-25 22:28:39 +0800

    submition 2

---
+++ src/animate.js
@@ -0,0 +1,27 @@
+
+
+function animation(obj, target,callBack) {
+            // console.log(callBack);
+            
+
+
+
+            clearInterval(obj.timer) //清除以前的定时器
+            obj.timer = setInterval(function () {
+                var step = (target - obj.offsetLeft)/10 //步长值修改为整数,向上取整
+             step =   step >0 ? Math.ceil(step) : Math.floor(step)
+
+
+                if (obj.offsetLeft == target) {
+                    clearInterval(obj.timer);
+                    //回调函数写在定时器结束里面
+                    if(callBack){
+                        callBack();
+
+                    }
+                }
+                obj.style.left = obj.offsetLeft + step + 'px';//缓动是把每次位移变小
+
+            }, 20)
+
+        }
