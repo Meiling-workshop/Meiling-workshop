5282141af20cce37fb20c93edb16694256854a56
Helen <helen.luanml@gmail.com>
2025-06-29 23:23:07 +0800

    added slide animation in 淘宝固定侧边栏

--- src/animate.js
+++ src/animate.js
@@ -8,7 +8,7 @@ function animation(obj, target,callBack) {
 
             clearInterval(obj.timer) //清除以前的定时器
             obj.timer = setInterval(function () {
-                var step = (target - obj.offsetLeft)/10 //步长值修改为整数,向上取整
+                var step = (target - obj.offsetLeft)/5 //步长值修改为整数,向上取整
              step =   step >0 ? Math.ceil(step) : Math.floor(step)
 
 
@@ -16,12 +16,13 @@ function animation(obj, target,callBack) {
                     clearInterval(obj.timer);
                     //回调函数写在定时器结束里面
                     if(callBack){
-                        callBack();
+                        callBack&&callBack();
 
                     }
                 }
                 obj.style.left = obj.offsetLeft + step + 'px';//缓动是把每次位移变小
-
-            }, 20)
+                // window.scroll(0, window.pageYOffset + step)
+            
+            }, 5)
 
         }
