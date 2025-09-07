aacace5af4e5ca50fac9f08a8d52c37034eb010f
Helen <helen.luanml@gmail.com>
2025-09-07 23:32:47 +0800

    no message

--- src/classAndObject/js/tab.js
+++ src/classAndObject/js/tab.js
@@ -19,12 +19,15 @@ class Tab{
             this.lis[i].index = i;
             this.lis[i].onlick = this.toggleTab
             this.remove[i].onlick = this.removeTab
+            this.span[i].ondblclick = this.editTab;
         }
     }
     updateNode(){
         this.lis = this.main.querySelectorAll('li')
         this.sections = this.main.querySelectorAll('section')
         this.remove = this.main.querySelectorAll('.icon-guanbi')
+        this.span = this.main.querySelectorAll('.firstnav li span:first-child')
+        // this.sections = this.main.querySelectorAll('section')
     }
      //1.切换功能
         toggleTab(){
@@ -51,15 +54,42 @@ class Tab{
             that.Init();
         }
         //3.删除功能
-        removeTab(){
+        removeTab(e){
             e.stopPropagation();
             var index = this.parentNode.index;
+            that.lis[index].remove();
+            that.section[index].remove();
+            that.Init();
+            if(document.querySelector('.liative')) return;
+            index--;
+            that.lis[index] && that.lis[index].click();
 
 
 
         }
         //4.修改功能
-        editTab(){}
+        editTab(){
+            var str = this.innerHTML;
+//禁止双击
+          window.getSelection ? window.getSelection().removeAllRanges : document.section.empty(); 
+          this.innerHTML = '< input type = "text" />' ;
+          var input = this.children[0];
+          input.value = str;
+          input.select();//文本处于选定状态
+          //当失去焦点文本框中的文字给span
+          input.onblur = function(){
+            this.input.parentNode.innerHTML = this.value;
+            //当enter焦点文本框中的文字给span
+            input.onkeyup = function(e){
+                if (e.keyCode === 13 ){
+                    this.onblur();
+                }
+
+            }
+          }
+          
+        
+        }
 }
 
 var tab = new Tab('#tab')
